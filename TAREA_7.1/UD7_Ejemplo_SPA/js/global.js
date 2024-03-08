bed6d6b8c47eae2f0c8d707fa330d12eb462d6a1
lenvigo <lenvigo@gmail.com>
2024-03-09 00:18:05 +0100

    reparacion

--- TAREA_7.1/UD7_Ejemplo_SPA/js/global.js
+++ TAREA_7.1/UD7_Ejemplo_SPA/js/global.js
@@ -1,7 +1,9 @@
 
-const base_url = "http://localhost:3000/TAREA_7.1/UD7_Ejemplo_SPA/controller/FrontController.php";
+//const base_url = "http://localhost:3000/TAREA_7.1/UD7_Ejemplo_SPA/controller/FrontController.php";
+const base_url = "http://localhost:3000/UD7ytarea/CS_UD7/TAREA_7.1/UD7_Ejemplo_SPA/controller/FrontController.php";
+
 //1. Crea  una variable global de javascript userId y almacena en ella  el userId que se recibe cuando se ha logrado un login exitoso en la parte cliente. Si no, su valor debe ser null.
-userId = null;
+var userId = null;
 window.onload = onceLoaded;
 
 
